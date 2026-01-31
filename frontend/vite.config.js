5c80bf78a83eb81c5f7a8930bd46111416bd6c5a
Michael Condon <michael.condon@interworks.nl>
2026-01-31 20:26:38 +0100

    changed frontend code to work with nginx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,17 +9,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: true,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5173,
+  //   allowedHosts: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: process.env.VITE_API_URL || 'http://localhost:5000',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   test: {
     globals: true,
     environment: 'jsdom',
